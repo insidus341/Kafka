1b2bfeb58ca991e41cc00a4e8838ae69064e3e32
James.Earl <james.earl@meraki.net>
2020-03-05 16:01:24 +0000

    Changed the get_cisco_stock_price script into a class

--- run/get_cisco_stock_price.py
+++ run/get_cisco_stock_price.py
@@ -5,58 +5,60 @@ import datetime
 # environmental variables are added in github
 import os
 
+class API_Get_Stock():
 
-def run():
+    def get_stock_price(self):
 
-    try:
-        response = get_api_request(os.environ['STOCK_API_KEY'])
-        response_with_timestamp = add_timestamp_to_response(response)
-        response_with_otherdata = add_otherdata_to_response(response_with_timestamp)
-    except:
-        # we don't need to anything if this fails
-        return False
+        try:
+            response = self.get_api_request(os.environ['STOCK_API_KEY'])
+            response_with_timestamp = self.add_timestamp_to_response(response)
+            response_with_otherdata = self.add_otherdata_to_response(response_with_timestamp)
+        except:
+            # we don't need to anything if this fails
+            return False
 
-    # send this data to our kafka topic
-    send_to_kafka(response_with_otherdata)
+        # send this data to our kafka topic
+        self.send_to_kafka(response_with_otherdata)
 
+    def _get_api_request(self, api_key):
+        response = requests.get(
+            "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=CSCO&interval=5min&apikey=" + api_key)
 
-def get_api_request(api_key):
-    response = requests.get(
-        "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=CSCO&interval=5min&apikey=" + api_key)
-
-    # 200 indicates this was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise ValueError()
-
-
-def add_timestamp_to_response(data):
-    # this data does not include a timestamp, let's add one
-    timestamp = datetime.datetime.now().timestamp()
-    data["timestamp"] = timestamp
-
-    return data
+        # 200 indicates this was successful
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ValueError()
 
 
-def add_otherdata_to_response(data):
-    data["otherdata"] = "hello this is a test"
+    def _add_timestamp_to_response(self, data):
+        # this data does not include a timestamp, let's add one
+        timestamp = datetime.datetime.now().timestamp()
+        data["timestamp"] = timestamp
 
-    return data
+        return data
 
 
-def send_to_kafka(data):
-    producer.send('API-CiscoStock', value=data)
+    def _add_otherdata_to_response(self, data):
+        data["otherdata"] = "hello this is a test"
+
+        return data
+
+
+    def _send_to_kafka(self, data):
+        producer.send('API-CiscoStock', value=data)
 
 
 if __name__ == "__main__":
-    # included here as it's not available on github
+    # included here as it's not available for Github CI/CD
     from kafka import KafkaProducer
 
     # create a handler for our kafka producer
+    # makes this global
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                              value_serializer=lambda x:
                              dumps(x).encode('utf-8'))
 
     # lets go
-    run()
+    kafka_api_cisco_stock = API_Get_Stock()
+    API_Get_Stock.run()
--- tests/test_get_cisco_stock_price.py
+++ tests/test_get_cisco_stock_price.py
@@ -1,15 +1,23 @@
 from run import get_cisco_stock_price
 import os
 
+
 def test_get_api_request():
-    out = get_cisco_stock_price.get_api_request(os.environ['STOCK_API_KEY'])
+    kafka_api_get_cisco_stock = get_cisco_stock_price.API_Get_Stock()
+    print("KEY")
+    print(os.environ['STOCK_API_KEY'])
+
+    out = kafka_api_get_cisco_stock._get_api_request(os.environ['STOCK_API_KEY'])
     assert out is not False
 
 
 def test_add_timestamp_to_response():
+    # Create some example data to add
     data = {
         "key": "value"
     }
 
-    out = get_cisco_stock_price.add_timestamp_to_response(data)
+    kafka_api_get_cisco_stock = get_cisco_stock_price.API_Get_Stock()
+
+    out = kafka_api_get_cisco_stock._add_timestamp_to_response(data)
     assert out['timestamp'] >= 1
